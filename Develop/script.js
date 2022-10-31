ca96cb68cc546fbd7ba6d1f6d9e076db4efdc569
Nicholas Bonner <nicholaslbonner@yahoo.com>
2022-10-31 01:55:21 -0700

    added if statement for password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,30 @@
 // Assignment code here
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789"
-var specialCharacters = "~`!@#$%^&*()-_=+\|]}[{'":;?/>.<,";
+var specialCharacters = "~`!@#$%^&*()-_=+\|]}[{';:?/>.<,";
+
+// var options = {
+//   lowerCase: abcdefghijklmnopqrstuvwxyz,
+//   upperCase: ABCDEFGHIJKLMNOPQRSTUVWXYZ,
+//   numbers: 0123456789,
+//   specialCharacters: ~`!@#$%^&*()-_=+\|]}[{';:?/>.<,;
+// };
 
 function generatePassword() {
   // console.log("clicked")
+  var userInput = window.prompt("How many characters would you like your password to be?");
+
+  // console.log("userInput")
+
+  var passwordLength = (userInput);
+
+  if (passwordLength > 8 || passwordLength > 128) {
+    window.alert("password must be greater than 8 characters and less than 128 characters.")
+    return
+  }
+
+
 };
 
 
