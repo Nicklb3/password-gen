ad38a38973de310f59c192675fe8ac56c4414823
Nicholas Bonner <nicholaslbonner@yahoo.com>
2022-10-31 22:43:36 -0700

    moved html file and updated link to css and js files

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,57 @@
 // Assignment code here
+
+// arrays I need for computer to have options to generate a password
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var specialCharacters = ["`","~","!","@","#","$","%","^","&","*","(",")","_","-","+","=","[","{","]","}","|",":",";","'","/","?",".",">",",","<"];
 
+// empty array to pass options
 var compOptions = [];
 
-// var options = {
-//   lowerCase: abcdefghijklmnopqrstuvwxyz,
-//   upperCase: ABCDEFGHIJKLMNOPQRSTUVWXYZ,
-//   numbers: 0123456789,
-//   specialCharacters: ~`!@#$%^&*()-_=+\|]}[{';:?/>.<,;
-// };
 
+// function to generate a password
 function generatePassword() {
   // console.log("clicked")
   var userInput = window.prompt("How many characters would you like your password to be?");
 
   // console.log("userInput")
 
+  // variable for length of password
   var passwordLength = (userInput);
 
+  // password length if statement
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("password must be greater than 8 characters and less than 128 characters.")
     return
   }
 
+  // varaibles for window confirm
   var useLowerCase = window.confirm("Do you want to use lowercase letters?")
   var useUpperCase = window.confirm("Do you want to use uppercase letters?")
   var useNumbers = window.confirm("Do you want to use numbers?")
   var useSpecialCharacters = window.confirm("Do you want to use special characters?")
   
-  var index = Math.floor(Math.random() * options.length);
-  var writePassword = options[index];
-
+  // if statments to push variables
   if (useLowerCase === true) {
     compOptions.push(lowerCase)
   };
-
+  
   if (useUpperCase === true) {
     compOptions.push(upperCase)
   };
-
+  
   if (useNumbers === true) {
     compOptions.push(numbers)
   };
-
+  
   if (useSpecialCharacters === true) {
     compOptions.push(specialCharacters)
   };
-
-
+  
+  
+  var index = Math.floor(Math.random() * options.length);
+  var writePassword = options[index];
 
 };
 
